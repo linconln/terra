fc44f2e5b317d09d038db6857141288d3685e742
Linconln Tadao Yamachita <linconln@gmail.com>
2024-05-22 17:42:32 -0300

    Projeto final - Terraform

--- projeto/security-group.tf
+++ projeto/security-group.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "ecs_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
